03463cca0c80be8039f405064beb4596481f2250
Habib <habib.kasbati@gmail.com>
2021-12-08 19:14:31 +0500

    Card Screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import TabNavigation from './src/Navigation/TabNavigation';
+export default function App() {
+  return (
+    <NavigationContainer >
+      <TabNavigation />
+      </NavigationContainer>
+  );
+}
+
+
