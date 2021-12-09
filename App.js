4d567b03990dc4ab671d41cd4e46378595163b82
Habib <habib.kasbati@gmail.com>
2021-12-09 18:38:27 +0500

    Card Game UI

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import TabNavigation from './src/Navigation/TabNavigation';
+
 export default function App() {
   return (
     <NavigationContainer >
@@ -9,5 +9,6 @@ export default function App() {
       </NavigationContainer>
   );
 }
+
 
 
---
+++ src/Constant/index.js
@@ -0,0 +1,5 @@
+
+export const cardHeight = 50;
+
+export const cardTitle = 45;
+export const cardPadding = 10;
